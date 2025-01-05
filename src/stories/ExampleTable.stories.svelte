f22848393b785431eac481563cdba691bd778325
Ian Anderson <35787432+andersonyyc@users.noreply.github.com>
2025-01-05 14:35:09 -0700

    Added example table and data

---
+++ src/stories/ExampleTable.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import ExampleTable from '$lib/ExampleTable.svelte';
+  
+  const { Story } = defineMeta({
+    title: 'Example Table',
+    component: ExampleTable,
+    tags: ['autodocs'],
+  });
+</script>
+
+<!-- Example table with default styling from tailwind and light branding -->
+<Story name="Primary" />
