d4e3a6481721b8f97f52959fb0a2b667ee4c480f
Maneet Goyal <maneetgoyal14@gmail.com>
2025-01-13 22:47:37 +0530

    refactor: remove label

--- src/lib/Quotas.svelte
+++ src/lib/Quotas.svelte
@@ -4,26 +4,22 @@
 
 <div class="overflow-hidden rounded-lg bg-white shadow">
   <div>
-    <div class="px-2">
-      <label for="search" class="block text-sm/6 font-medium text-gray-900"
-        >Quick search</label
+    <div class="p-2">
+      <div
+        class="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
       >
-      <div class="mt-2">
-        <div
-          class="flex rounded-md bg-white outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600"
-        >
-          <input
-            type="text"
-            name="search"
-            id="search"
-            class="block min-w-0 grow rounded-s-md border-gray-300 px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
-          />
-          <div class="flex p-1.5">
-            <kbd
-              class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400"
-              >⌘K</kbd
-            >
-          </div>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search..."
+          class="block min-w-0 grow rounded-s-md border-gray-300 px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6"
+        />
+        <div class="flex p-1.5">
+          <kbd
+            class="inline-flex items-center rounded border border-gray-200 px-1 font-sans text-xs text-gray-400"
+            >⌘K</kbd
+          >
         </div>
       </div>
     </div>
