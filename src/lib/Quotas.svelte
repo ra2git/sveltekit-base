608afa660ce1455226163c5610eb70ca2e290254
Maneet Goyal <maneetgoyal14@gmail.com>
2025-01-15 08:45:03 +0530

    feat: Add edit button

--- src/lib/Quotas.svelte
+++ src/lib/Quotas.svelte
@@ -169,7 +169,11 @@
                       >
                       <td
                         class="whitespace-nowrap pl-4 pr-3 text-sm text-gray-500"
-                        >Edit</td
+                        ><button
+                          type="button"
+                          class="rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                          >Edit</button
+                        ></td
                       >
                     </tr>
                   {/each}
